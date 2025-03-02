3475766feb157e0a343a9cae05fdc456c1ff9858
Julian Rupprecht <julianrupprecht06@gmail.com>
2025-03-02 20:04:36 +0100

    Implemented timer feature

--- website/views.py
+++ website/views.py
@@ -11,15 +11,26 @@ views = Blueprint('views', __name__)
 def home():
     if request.method == 'POST':
         data = request.form.get('data')
-        
         if len(data) < 1:
             flash('To-Do must contain atleast 1 character', category='error')
         else:
             new_task = Task(data=data, user_id=current_user.id)
             db.session.add(new_task)
             db.session.commit()
-            flash('Added To-Do', category='success')
 
+            tasks = Task.query.filter_by(user_id=current_user.id)
+            data = []
+            for task in tasks: 
+                data.append(
+                    {
+                        "id": task.id,
+                        "data": task.data,
+                        "date": task.date,
+                        "user_id": task.user_id
+                    }
+                )
+            return jsonify(data)
+        
     return render_template('home/home.html', user=current_user)
 
 @views.route('/delete-task', methods=["POST"])
@@ -33,8 +44,18 @@ def delete_task():
         if task.user_id == current_user.id:
             db.session.delete(task)
             db.session.commit()
-            flash('Task deleted', category='success')
-            return jsonify({})
+            tasks = Task.query.filter_by(user_id=current_user.id)
+            data = []
+            for task in tasks: 
+                data.append(
+                    {       
+                        "id": task.id,
+                        "data": task.data,
+                        "date": task.date,
+                        "user_id": task.user_id
+                    }
+                )
+            return jsonify(data)
         
     flash('Error while trying to delete task', category='error')
     return jsonify({})
